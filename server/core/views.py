62c9f23a9fc406b6d58a7b0ec31ebfdc694d48c1
vzhuhfaka <ostanin20.06.a@gmail.com>
2025-03-10 11:25:25 +0300

    Добавлены сериализаторы к модели

--- server/core/views.py
+++ server/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+from django.forms import model_to_dict
+from .serializers import UserSerializer
 
-# Create your views here.
+class VideoAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        return Response({'users': UserSerializer(users, many=True).data})
+    
+    def post(self, request):
+        post_new = User.objects.create(
+            username=request.data['username'],
+            password=request.data['password'],
+            email=request.data['email'],
+            created_at=request.data['created_at'],
+            updated_at=request.data['updated_at']
+        )
+        return Response({'post': model_to_dict(post_new)})
--- server/server/urls.py
+++ server/server/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import VideoAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v0.1/video', VideoAPIView.as_view())
 ]
