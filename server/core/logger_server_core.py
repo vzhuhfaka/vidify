f84ada2337454281759064b42c247b13ce1bda24
vzhuhfaka <ostanin20.06.a@gmail.com>
2025-03-25 23:06:09 +0300

    issue#13 выполнено

---
+++ server/core/logger_server_core.py
@@ -0,0 +1,54 @@
+import logging
+
+
+def clear_handlers(logger):
+    '''
+    Удаление всех обработчиков у логгера
+    '''
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+
+def init_logger(name):
+    # Настройка логгера
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
+
+    # Удаляем старые обработчики
+    clear_handlers(log)
+
+    # Обработчик для записи в файл
+    file_handler = logging.FileHandler('server/core/server_log.log')
+    file_handler.setLevel(logging.INFO)
+
+    # Форматтер
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Добавление обработчика
+    log.addHandler(file_handler)
+    return log
+
+def info(name, msg):
+    log = init_logger(name)
+    log.info(msg)
+
+def debug(name, msg): 
+    log = init_logger(name)
+    log.debug(msg)
+
+def warning(name, msg):
+    log = init_logger(name)
+    log.warning(msg)
+
+def error(name, msg):
+    log = init_logger(name)
+    log.error(msg)
+
+def cricital(name, msg):
+    log = init_logger(name)
+    log.critical(msg)
+
+    
+# Экспорт
+__all__ = [info, debug, warning, error, cricital]
