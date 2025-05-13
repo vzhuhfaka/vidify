c71a08238d21a2edaee6b9b54b5c5a015e1abbc7
vzhuhfaka <ostanin20.06.a@gmail.com>
2025-05-13 22:46:18 +0300

    Баг фикс

--- vidify_server/core/api/v2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from core.api.v2.views import VideoAPIView, LikeAPI
-from django.urls import path
-
-
-urlpatterns = [
-    path('video/<int:pk>', VideoAPIView.as_view()),
-    path('set-like', LikeAPI.as_view())
-]
