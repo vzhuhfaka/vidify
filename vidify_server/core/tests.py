3b092a229937e4f4982cc68788058e99deb86ecf
vzhuhfaka <ostanin20.06.a@gmail.com>
2025-05-17 13:22:09 +0300

    Выполнен task.20 и task.21, а также дописаны тесты(task.19)

--- vidify_server/core/tests.py
+++ vidify_server/core/tests.py
@@ -34,6 +34,33 @@ class UserVideoAPITestCase(TestCase):
         self.assertEqual(user_videos.status_code, 200)
 
 
+def create_user_and_video(client):
+    data_for_reg = {
+        'username': 'testuser',
+        'password': 'testpassword'
+    }
+    client.post('/api/v3/register', data_for_reg)
+
+    data_for_create_video = {
+        'video_file': open('vidify_server/core/test_files/test_video.mp4', 'rb'),
+        'preview': open('vidify_server/core/test_files/test_preview.jpg', 'rb'),
+        'title': 'Test Video',
+        'description': 'Test Description',
+        'views': 0,
+        'likes': 0,
+        'user': 1,
+    }
+    client.post('/api/v3/add-video', data_for_create_video)
+
+def create_like_for_video(client):
+    data_for_create_like = {
+        'user_id': 1,
+        'video_id': 1,
+        'new_likes': 100
+    }
+    client.post('/api/v3/set-like', data_for_create_like)
+
+
 class VideoAPITestCase(TestCase):
     def test_post_create_video(self):
         """Тест на создание видео"""
@@ -53,11 +80,17 @@ class VideoAPITestCase(TestCase):
     def test_get_one_video(self):
         """Тест на получение одного видео по id"""
         client = Client()
-        one_video = client.get('/api/v3/video-by-id/1')
-        many_videos = client.get('/api/v3/all-video')
+        
+        # Сначала создаем пользователя и видео для теста
+        create_user_and_video(client)
+
+        one_video = client.post('/api/v3/video-by-id', {
+            'video_id': 1,
+            'user_id': 1
+        })
+
         self.assertEqual(one_video.status_code, 200)
-        self.assertEqual(many_videos.status_code, 200)
-    
+
     def test_get_many_videos(self):
         """Тест на получение всех видео"""
         client = Client()
@@ -69,3 +102,33 @@ class VideoAPITestCase(TestCase):
         client = Client()
         delete_video = client.delete('/api/v3/video-by-id/1')
         self.assertEqual(delete_video.status_code, 200)
+
+
+class LikeAPITestCase(TestCase):
+    def test_post_set_like(self):
+        """Тест на добавление лайка к видео"""
+        client = Client()
+        # Создаем пользователя и видео для теста
+        create_user_and_video(client)
+        data = {
+            'user_id': 1,
+            'video_id': 1,
+            'new_likes': 100
+        }
+        req = client.post('/api/v3/set-like', data)
+        self.assertEqual(req.status_code, 200)
+
+    def test_delete_set_like(self):
+        """Тест на удаление лайка к видео"""
+        client = Client()
+        # Создаем пользователя и видео для теста
+        create_user_and_video(client)
+        create_like_for_video(client)
+
+        data = {
+            'user_id': 1,
+            'video_id': 1,
+            'new_likes': 98
+        }
+        req = client.delete('/api/v3/set-like', data, content_type='application/json')
+        self.assertEqual(req.status_code, 200)
